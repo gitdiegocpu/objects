2183c7a9a1b6b7c5b09c99e2b0a7c8724749a8a6
Diego F Garcia <gadifer54@gamil>
2023-10-05 16:55:18 -0400

    Objects

---
+++ index.js
@@ -0,0 +1,23 @@
+const person = { //  Declare a new object 
+    firstName:"todd",
+    age: 28
+}
+// print the person's name
+// console.log(person.firstName)
+
+// add one to the person's age 
+person.age++
+
+// console.log(person.age)
+
+person.lastName = "Albert"  // add a new property to the object 
+console.log(person.firstName, person.lastName, person.age)
+/* 
+const person = {
+    firstName: " Todd "
+    age: 29 
+    lastName: "Albert"
+}
+*/
+
+console.log(person)
